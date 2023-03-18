204d438d29974dd83404a9e430000f4fef60350e
erichayth <42790061+erichayth@users.noreply.github.com>
2023-03-17 23:42:00 -0400

    updated pages folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,9 +34,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
     '@nuxtjs/sanity'
-
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
